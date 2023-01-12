5874882fefa8bf9bd9fc7bd70773774d3800592b
Artem007 <artem.zagrebelny@yandex.ru>
2023-01-12 03:41:34 +0300

    Added Method4

---
+++ Вид 4/Program.cs
@@ -0,0 +1,13 @@
+﻿string Metod4(int count, string text)
+{
+int i = 0;
+string result = String.Empty;
+while (i<count)
+{
+result = result + text;
+i++;
+}
+return result;
+}
+string res = Metod4(10, "Ура! Работает!");
+Console.WriteLine(res);
