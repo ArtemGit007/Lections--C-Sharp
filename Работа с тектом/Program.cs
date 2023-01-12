ad7a25f48424a6e4c6905eb3459b4d66083a54ac
Artem007 <artem.zagrebelny@yandex.ru>
2023-01-12 04:16:43 +0300

    Added variant of the changing later К

--- Работа с тектом/Program.cs
+++ Работа с тектом/Program.cs
@@ -18,7 +18,24 @@ string text = "— Я думаю, — сказал князь, улыбаясь,
 // Например: 
 // s[3] // r
 
- string Replace(string text, char oldValue, char newValue)
+ //string Replace(string text, char oldValue, char newValue)
+//{
+//    string result = String.Empty;
+
+//    int length = text.Length;
+//    for (int i = 0; i < length; i++)
+ //   {
+ //        if(text[i] == oldValue) result = result + $"{newValue}";
+ //        else result = result + $"{text[i]}";
+ //   }
+
+ //   return result;
+//}
+
+//string newText = Replace(text, ' ', '|' );
+//Console.WriteLine(newText);
+
+string Replace(string text, char oldValue, char newValue)
 {
     string result = String.Empty;
 
@@ -32,5 +49,5 @@ string text = "— Я думаю, — сказал князь, улыбаясь,
     return result;
 }
 
-string newText = Replace(text, ' ', '|' );
+string newText = Replace(text, 'к', 'К');
 Console.WriteLine(newText);
